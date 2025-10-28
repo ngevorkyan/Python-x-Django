4daec038df5f36b73b9f4053a16501d2b6d48cd0
Nino Gevorkyan <gevorkyan.nino@gmail.com>
2025-10-28 21:30:33 +0400

    Add three restaurant instances and descriptions
    Created instances of Restaurant class and called describe_restaurant() for each.

---
+++ ex9-2.py
@@ -0,0 +1,14 @@
+"""
+9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three
+different instances from the class, and call describe_restaurant() for each
+instance.
+"""
+
+french_restaurant = Restaurant('Bonjour', 'French')
+italian_restaurant= Restaurant('La Dolce Vita', 'Italian')
+caribbean_restaurant = Restaurant('Sunset', 'Caribbean')
+
+
+french_restaurant.describe_restaurant()
+italian_restaurant.describe_restaurant()
+caribbean_restaurant.describe_restaurant()
